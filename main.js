f05f19af05d500bdb95ccd1616afdbfe9e2c10fd
chuanhtoan <chuanhtoan139@gmail.com>
2021-01-27 10:54:11 +0700

    Add back to top button

---
+++ main.js
@@ -0,0 +1,14 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        document.getElementById("back-to-top").style.opacity = "1";
+    } else {
+        document.getElementById("back-to-top").style.opacity = "0";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
